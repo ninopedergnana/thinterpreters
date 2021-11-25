bab37fb235cb182ddc51c439f22bd71687c3cfca
pedernin <pedernin@students.zhaw.ch>
2021-11-25 17:43:28 +0100

    deleted unecessary const

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT  || 3001
-const baseUrl = process.env.ENV_URL || `http://localhost:${port}`;
 const cors = require('cors');
 
 app.use(cors())
