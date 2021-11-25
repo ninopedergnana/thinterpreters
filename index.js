c934bb65861dbe884e6310d2feb9dd60af163c49
pedernin <pedernin@students.zhaw.ch>
2021-11-25 18:25:26 +0100

    port to PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const port = process.env.PORT  || 3001
+const PORT = process.env.PORT  || 3001
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
