f2088f6b59c03741144f9f0083202894e0dc22c4
pedernin <pedernin@students.zhaw.ch>
2021-12-16 12:03:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function run(code, params) {
     stdio: 'pipe',
   })
   if (child.status === null) {
-    return `{error: "Process terminated with exit code 1! Maybe you implemented an endless loop."}`
+    return `{"error": "Process terminated with exit code 1! Maybe you implemented an endless loop."}`
   }
   return child.stdout.toString()
 }
