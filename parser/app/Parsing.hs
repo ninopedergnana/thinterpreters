1b64c276e6dc7b552b7002dd9fa3d6f2a94888d1
pedernin <pedernin@students.zhaw.ch>
2021-12-23 14:17:32 +0100

    removed comments

--- parser/app/Parsing.hs
+++ parser/app/Parsing.hs
@@ -22,13 +22,9 @@ sc = L.space space1 lineCmnt empty
   where
     lineCmnt  = L.skipLineComment "//"
 
--- lexeme is a wrapper for lexemes that 
--- picks up all trailing white space using the supplied space consumer.
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
 
--- symbol is a parser that matches given text using string internally 
--- and then similarly picks up all trailing white space.
 symbol :: String -> Parser String
 symbol = L.symbol sc
 
