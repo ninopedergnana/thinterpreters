a67bcf1270dd5f00349be3ad1ff1b74012162db9
pedernin <pedernin@students.zhaw.ch>
2022-04-10 00:39:05 +0200

    update doc

--- public/scripts/documentation.js
+++ public/scripts/documentation.js
@@ -31,4 +31,18 @@ document.addEventListener('DOMContentLoaded', () => {
         lineNumbers: false,
         readOnly: true
     });
+
+    codeMirrorReadOnly = CodeMirror(document.getElementById('documentation6'), {
+        value: 'While variable > 0 Do\n    program (subroutine)\nEnd',
+        mode: 'goto',
+        lineNumbers: false,
+        readOnly: true
+    });
+
+    codeMirrorReadOnly = CodeMirror(document.getElementById('documentation7'), {
+        value: 'While x2 > 0 Do     // Header for while program w1\n  x4 = x2 + 0;      // Assignment, w1 subroutine\n  x0 = x1 + 0;      // Assignment, w1 subroutine\n  Loop x1 Do        // Header for loop program l1\n    x1 = x1 - x2    // Assignment, l1 subroutine\n  End               // End of l1\nEnd;                // End of w1\nx0 = x1 + 0         // Assignment',
+        mode: 'goto',
+        lineNumbers: false,
+        readOnly: true
+    });
 })
